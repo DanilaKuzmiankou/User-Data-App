55bed3d534274f79ab736c58e02cc60ccd5c223a
St4ffer <stmail468@gmail.com>
2022-02-16 20:11:57 +0300

    /

--- db.js
+++ db.js
@@ -1,11 +1,9 @@
 const {Sequelize} = require('sequelize')
 
 module.exports = new Sequelize(process.env.DATABASE_URL, {
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        }
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
     }
-);
+});
