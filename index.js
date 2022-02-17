9c8ed00297c3ea39e18f2c774d280ae5036a013d
St4ffer <stmail468@gmail.com>
2022-02-17 17:34:52 +0300

    /

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const app = express()
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
-
+    app.get('/userPage', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
 }
 
 
